2d1a0965dfdebe6a4b6075e32450188701afd2c2
lipothviki <lipoth.viki@gmail.com>
2022-09-26 20:18:07 +0200

    add_signin_page

---
+++ src/components/SignInPage/SignInPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInPage = () => {
+    return <div className="container d-flex justify-content-center">
+        <div className="card my-4  p-4 bg-white shadow text-center">
+            <h5><b>Sign In Page</b></h5>
+        </div>
+    </div>
+};
+
+export default SignInPage;
